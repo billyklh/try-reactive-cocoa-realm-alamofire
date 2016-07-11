b3dfeaf284f6cce038080e2c7dacb5e93c72613d
billykong <billyklh@gmail.com>
2016-07-12 00:18:09 +0800

    Initial Commit

---
+++ RacRealmAlamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RacRealmAlamofire
+//
+//  Created by Kong Lim Ho on 12/7/2016.
+//  Copyright © 2016 BillyKong@Home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
